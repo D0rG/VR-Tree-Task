eb6bfa8b8d3b263fd250db461e58e0d0dd963ec0
Mixail <mixail.korchikov@mail.ru>
2022-10-30 19:05:05 +0300

    Firs variant

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class Interaction : MonoBehaviour
 {
@@ -11,7 +12,20 @@ public class Interaction : MonoBehaviour
     private bool _leftGripBefore = false;
     private bool _rightGripBefore = false;
     private Vector3 _size = Vector3.one;
-    private float controllerDistanceMax = 0;
+    private float _controllerDistanceMax = 0;
+    private Transform _sphereTransform;
+    private Transform _pointToRotate;
+
+    private void Awake()
+    {
+        var sphere = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        sphere.GetComponent<MeshRenderer>().enabled = false;
+        sphere.GetComponent<BoxCollider>().enabled = false;
+        _sphereTransform = sphere.GetComponent<Transform>();
+        _sphereTransform.localScale = Vector3.one * 0.2f;
+        _sphereTransform.parent = _leftControllerInfo.transform;
+        _sphereTransform.position = _leftControllerInfo.transform.position;
+    }
 
     private void Update()
     {
@@ -24,22 +38,62 @@ public class Interaction : MonoBehaviour
 
         if (_leftControllerInfo.gripState && _rightControllerInfo.gripState)
         {
+            Look();
+            ConnectTree();
             if (_isFirstDoublegrip)
             {
-                Debug.Log("Оба контроллера сжаты в первый раз");
-                controllerDistanceMax = controllerDistance;
+                _controllerDistanceMax = controllerDistance;
                 _size = _treeObject.transform.localScale;
             }
             else
             {
-                var procent = controllerDistance / controllerDistanceMax;
+                var procent = controllerDistance / _controllerDistanceMax;
                 _treeObject.transform.localScale = _size * procent;
             }
         }
-
+        else if(_leftControllerInfo.gripState)
+        {
+            _leftGripBefore = true;
+            SetupConnection(_leftControllerInfo, _rightControllerInfo.transform);
+        }
+        else if(_rightControllerInfo.gripState)
+        {
+            _rightGripBefore = true;
+            SetupConnection(_rightControllerInfo, _leftControllerInfo.transform);
+        }
+        else
+        {
+            DisconnectTree();
+            _leftGripBefore = false;
+            _rightGripBefore = false;
+        }
 
         _leftGripBefore = _leftControllerInfo.gripState;
         _rightGripBefore = _rightControllerInfo.gripState;
         _isFirstDoublegrip = !(_leftControllerInfo.gripState && _rightControllerInfo.gripState);
     }
+
+    private void Look()
+    {
+        _sphereTransform.LookAt(_pointToRotate);
+    }
+
+    private void SetupConnection(HandVRInput controller, Transform toRotatePoint)
+    {
+        DisconnectTree();
+        _sphereTransform.parent = controller.transform;
+        _sphereTransform.position = controller.transform.position;
+        _sphereTransform.rotation = Quaternion.identity;
+        _pointToRotate = toRotatePoint;
+    }
+
+    private void ConnectTree()
+    {
+        _treeObject.transform.parent = _sphereTransform;
+    }
+
+    private void DisconnectTree()
+    {
+        _treeObject.transform.parent = null;
+    } 
 }
